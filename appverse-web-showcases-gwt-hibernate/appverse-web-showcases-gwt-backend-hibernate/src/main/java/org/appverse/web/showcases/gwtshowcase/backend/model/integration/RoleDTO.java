4f67896758b4223821880cf2a7aa804d65cf2536
miguelfgar <miguelfgar@gmail.com>
2014-01-08 17:04:00 +0100

    Added comments

--- appverse-web-showcases-gwt-hibernate/appverse-web-showcases-gwt-backend-hibernate/src/main/java/org/appverse/web/showcases/gwtshowcase/backend/model/integration/RoleDTO.java
+++ appverse-web-showcases-gwt-hibernate/appverse-web-showcases-gwt-backend-hibernate/src/main/java/org/appverse/web/showcases/gwtshowcase/backend/model/integration/RoleDTO.java
@@ -11,74 +11,63 @@ import java.io.Serializable;
 @Entity
 @Table(name = "ROLE")
 public class RoleDTO extends AbstractIntegrationAuditedJPABean implements
-		Serializable {
+        Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private String name;
+    private String name;
 
-	private String description;
+    private String description;
 
-	private boolean active = true;
+    private boolean active = true;
 
-	public RoleDTO() {
-	}
+    public RoleDTO() {
+    }
 
-	@Column(nullable = true, length = 200)
-	public String getDescription() {
-		return description;
-	}
+    @Column(nullable = true, length = 200)
+    public String getDescription() {
+        return description;
+    }
 
-	@Id
-	@TableGenerator(name = "ROLE_GEN", table = "SEQUENCE", pkColumnName = "SEQ_NAME", pkColumnValue = "ROLE_SEQ", valueColumnName = "SEQ_COUNT", initialValue = 1, allocationSize = 1)
-	@GeneratedValue(strategy = GenerationType.TABLE, generator = "ROLE_GEN")
-	public long getId() {
-		return id;
-	}
+    @Id
+    @TableGenerator(name = "ROLE_GEN", table = "SEQUENCE", pkColumnName = "SEQ_NAME", pkColumnValue = "ROLE_SEQ", valueColumnName = "SEQ_COUNT", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "ROLE_GEN")
+    public long getId() {
+        return id;
+    }
 
-	@Column(nullable = false, unique = true, length = 40)
-	public String getName() {
-		return name;
-	}
+    @Column(nullable = false, unique = true, length = 40)
+    public String getName() {
+        return name;
+    }
 
-	// @Version implies activate JPA Optimistic Locking for this object
-	// Sometimes we will just annotate the parent entity of a more complex
-	// structure so that we can "lock" the full object taking
-	// into account a functional context. Then, when a child node is update we
-	// will force the full object version to change
-	// making the "lastUpdateBy" field = NULL. Then the EntityListener will
-	// update the object to add the last updated by field. (Another option is to
-	// annotate childs as well, but always force the update of the parent).
-	// This allows to detect that if several users are working with an object
-	// (with a complex structure) if the change something in the fields
-	// we can apply optimistic locking at full object level.
-	// Considerations: Batch updates and inserts does not work with entities
-	// annotated with @version.
-	// In this cases, the inserts and updates will be one at a time.
-	// In summary, we need to study every considering:
-	// 1. We want to "lock" (detect optimistic locking) at functional object
-	// level (not per entity) and use the strategy previously explained
-	// 2. We need to use batch / insert updates or not
-	@Override
-	@Version
-	public long getVersion() {
-		return version;
-	}
+    // @Version implies activate JPA Optimistic Locking for this object
+    /* Difference with EclipseLink
+       This does not work for Hibernate as it manages override of annotations in JPA entities different than Eclipselink.
+       Overriding this field and adding @Version for optimistic locking would not work (it does with EclipseLink).
+       For optimistic locking with Hibernate this has to be added directly in the project classes.
+   */
+    // This did not work: http://stackoverflow.com/questions/15092204/how-to-override-parameter-in-sublass-of-mappedsuperclass
+    @Override
+    @Version
+    public long getVersion() {
+        return version;
+    }
 
-	@Column(nullable = false)
-	public boolean isActive() {
-		return active;
-	}
+    @Column(nullable = false)
+    public boolean isActive() {
+        return active;
+    }
 
-	public void setActive(final boolean active) {
-		this.active = active;
-	}
+    public void setActive(final boolean active) {
+        this.active = active;
+    }
 
-	public void setDescription(final String description) {
-		this.description = description;
-	}
+    public void setDescription(final String description) {
+        this.description = description;
+    }
 
-	public void setName(final String name) {
-		this.name = name;
-	}
+    public void setName(final String name) {
+        this.name = name;
+    }
 }
--- appverse-web-showcases-gwt-hibernate/appverse-web-showcases-gwt-backend-hibernate/src/main/java/org/appverse/web/showcases/gwtshowcase/backend/model/integration/UserDTO.java
+++ appverse-web-showcases-gwt-hibernate/appverse-web-showcases-gwt-backend-hibernate/src/main/java/org/appverse/web/showcases/gwtshowcase/backend/model/integration/UserDTO.java
@@ -23,60 +23,59 @@
  */
 package org.appverse.web.showcases.gwtshowcase.backend.model.integration;
 
+import org.appverse.web.framework.backend.persistence.model.integration.AbstractIntegrationAuditedJPABean;
+
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.*;
-
-import org.appverse.web.framework.backend.persistence.model.integration.AbstractIntegrationAuditedJPABean;
-
 @Entity
 @Table(name = "USER")
 public class UserDTO extends AbstractIntegrationAuditedJPABean implements
-		Serializable {
+        Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     private String name;
-	private String lastName;
-	private String email;
-	private String password;
+    private String lastName;
+    private String email;
+    private String password;
 
     private List<RoleDTO> roles = new ArrayList<RoleDTO>();
 
-	private boolean active = true;
+    private boolean active = true;
 
 
-	public UserDTO() {
-	}
+    public UserDTO() {
+    }
 
-	@Column(nullable = false, unique = true, length = 40)
-	public String getEmail() {
-		return email;
-	}
+    @Column(nullable = false, unique = true, length = 40)
+    public String getEmail() {
+        return email;
+    }
 
-	@Id
-	@TableGenerator(name = "USER_GEN", table = "SEQUENCE", pkColumnName = "SEQ_NAME", pkColumnValue = "USER_SEQ", valueColumnName = "SEQ_COUNT", initialValue = 1, allocationSize = 1)
-	@GeneratedValue(strategy = GenerationType.TABLE, generator = "USER_GEN")
-	public long getId() {
-		return id;
-	}
+    @Id
+    @TableGenerator(name = "USER_GEN", table = "SEQUENCE", pkColumnName = "SEQ_NAME", pkColumnValue = "USER_SEQ", valueColumnName = "SEQ_COUNT", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "USER_GEN")
+    public long getId() {
+        return id;
+    }
 
-	@Column(nullable = false, length = 40)
-	public String getLastName() {
-		return lastName;
-	}
+    @Column(nullable = false, length = 40)
+    public String getLastName() {
+        return lastName;
+    }
 
-	@Column(nullable = false, length = 40)
-	public String getName() {
-		return name;
-	}
+    @Column(nullable = false, length = 40)
+    public String getName() {
+        return name;
+    }
 
-	@Column(nullable = false, length = 40)
-	public String getPassword() {
-		return password;
-	}
+    @Column(nullable = false, length = 40)
+    public String getPassword() {
+        return password;
+    }
 
     // unidirectional one-to-many association to ROLE
     @OneToMany(cascade = CascadeType.REFRESH)
@@ -85,37 +84,43 @@ public class UserDTO extends AbstractIntegrationAuditedJPABean implements
         return roles;
     }
 
-    // This does not work: http://stackoverflow.com/questions/15092204/how-to-override-parameter-in-sublass-of-mappedsuperclass
-	@Override
-	@Version
-	public long getVersion() {
-		return version;
-	}
+    // @Version implies activate JPA Optimistic Locking for this object
+    /* Difference with EclipseLink
+       This does not work for Hibernate as it manages override of annotations in JPA entities different than Eclipselink.
+       Overriding this field and adding @Version for optimistic locking would not work (it does with EclipseLink).
+       For optimistic locking with Hibernate this has to be added directly in the project classes.
+   */
+    // This did not work: http://stackoverflow.com/questions/15092204/how-to-override-parameter-in-sublass-of-mappedsuperclass
+    @Override
+    @Version
+    public long getVersion() {
+        return version;
+    }
 
-	@Column(nullable = false)
-	public boolean isActive() {
-		return active;
-	}
+    @Column(nullable = false)
+    public boolean isActive() {
+        return active;
+    }
 
-	public void setActive(final boolean active) {
-		this.active = active;
-	}
+    public void setActive(final boolean active) {
+        this.active = active;
+    }
 
-	public void setEmail(final String email) {
-		this.email = email;
-	}
+    public void setEmail(final String email) {
+        this.email = email;
+    }
 
-	public void setLastName(final String lastName) {
-		this.lastName = lastName;
-	}
+    public void setLastName(final String lastName) {
+        this.lastName = lastName;
+    }
 
-	public void setName(final String name) {
-		this.name = name;
-	}
+    public void setName(final String name) {
+        this.name = name;
+    }
 
-	public void setPassword(final String password) {
-		this.password = password;
-	}
+    public void setPassword(final String password) {
+        this.password = password;
+    }
 
     public void setRoles(final List<RoleDTO> roles) {
         this.roles = roles;
