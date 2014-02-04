e47786697d5f3315ae11ddd5ab4ee592e7ef511f
miguelfgar <miguelfgar@gmail.com>
2014-02-04 10:14:00 +0100

    Adding unit test to GWT Eclipselink showcase (work in progress)

---
+++ appverse-web-showcases-gwt-eclipselink/appverse-web-showcases-gwt-backend-eclipselink/src/test/java/org.appverse.web.showcases.gwtshowcase.backend/services/business/impl/test/UserServiceTestImpl.java
@@ -0,0 +1,149 @@
+package org.appverse.web.showcases.gwtshowcase.backend.services.business.impl.test;
+
+import org.appverse.web.framework.backend.api.helpers.test.AbstractTransactionalTest;
+import org.appverse.web.showcases.gwtshowcase.backend.model.business.Role;
+import org.appverse.web.showcases.gwtshowcase.backend.model.business.User;
+import org.appverse.web.showcases.gwtshowcase.backend.services.business.RoleService;
+import org.appverse.web.showcases.gwtshowcase.backend.services.business.UserService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+import java.util.List;
+
+public class UserServiceTestImpl extends AbstractTransactionalTest {
+
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private RoleService roleService;
+
+	private User user = new User();
+
+	@Before
+	public void initialize() throws Exception {
+		// Set first level fields
+		user.setStatus(User.STATUS_ACTIVE);
+		user.setEmail("email");
+		user.setLastName("lastName");
+		user.setName("name");
+		user.setPassword("password");
+        user.setCreated(new Date());
+        user.setCreatedBy("test");
+
+		// Add collections
+
+        // We add the 4 regular roles (as we have 5 we remove the extra that will be used later to test
+        // roles additions)
+        List<Role> roleCollection = roleService.loadRoles();
+        roleCollection.remove(4);
+
+        user.setRoles(roleCollection);
+	}
+
+    private void checkUser(User user) {
+        Assert.assertNotNull("Entity can't be null", user);
+
+        // Check collections
+        Assert.assertNotNull("Roles collection can't be null", user.getRoles());
+        Assert.assertTrue("Roles collection size must be 4", user.getRoles()
+                .size() == 4);
+    }
+
+    /*
+        Take into account in this method we do not check collections size as the
+        loadUsers() retrieves users using WithoutDependencies converter.
+        The reason is that usually when you retrieve all the list of objects you do not need all the details /
+        dependencies. For performance reasons, when you require this information for a particular object then
+        the required dependencies are retrieved.
+     */
+    private void checkUserList(List<User> list) {
+        Assert.assertNotNull("Entity list has not been able to be retrieved",
+                list);
+        Assert.assertTrue("Entity list is empty", list.size() > 0);
+    }
+
+	@Test
+	public void persistAndModifyNewUser() throws Exception {
+		// Persist new
+		final long resultPk = userService.saveUser(user);
+		Assert.assertNotEquals("The returned entity id can't be 0", 0L, resultPk);
+
+		// Retrieve
+		User userAux = userService.loadUser(resultPk);
+		checkUser(userAux);
+
+		// Modify and persist
+		userAux.setName("new");
+		userService.saveUser(userAux);
+		userAux = userService.loadUser(resultPk);
+		Assert.assertEquals("The value was not properly updated", "new",
+                userAux.getName());
+		checkUser(userAux);
+	}
+
+    @Test
+    public void removeFromUserDependencies() throws Exception{
+        // Element removal from collections check (converters remove-orphan test)
+
+        // Retrieve existing user (1)
+        User userAux = userService.loadUser(1L);
+        checkUser(userAux);
+
+        // Role dependency removal
+        long roleId = userAux.getRoles().get(0).getId();
+		userAux.getRoles().remove(0);
+
+        // Other dependencies removal....
+
+		long resultPk = userService.saveUser(userAux);
+
+        // We retrieve the user
+		userAux = userService.loadUser(resultPk);
+
+        // Role dependency check (we check a role has been removed)
+		Assert.assertEquals("The list of entities size is not correct", 3L,
+                userAux.getRoles().size());
+
+        // Other dependency check
+
+    }
+
+    @Test
+    public void addToUserDependencies() throws Exception{
+        // Element add to collections check (converters cummulative test)
+
+        // Retrieve existing user (1)
+        User userAux = userService.loadUser(1L);
+        checkUser(userAux);
+
+        // Role dependency addition (we add the extra role (5) to he user
+        userAux.getRoles().add(roleService.loadRole(5L));
+
+        // Other dependencies addition...
+        userService.saveUser(userAux);
+
+        // We retrieve the user
+        userAux = userService.loadUser(1L);
+
+        // Role dependency check (we check the extra role is added)
+        Assert.assertEquals("The list of entities size is not correct", 5L,
+                userAux.getRoles().size());
+
+        // Other dependency check
+
+
+    }
+
+
+    @Test
+	public void retrieveAllUsers() throws Exception {
+		final long resultPk = userService.saveUser(user);
+		Assert.assertNotSame("The returned entity id can't be 0", 0L, resultPk);
+
+		final List<User> result = userService.loadUsers();
+		checkUserList(result);
+	}
+}
---
+++ appverse-web-showcases-gwt-eclipselink/appverse-web-showcases-gwt-backend-eclipselink/src/test/java/org.appverse.web.showcases.gwtshowcase.backend/services/presentation/impl/test/UserServiceFacadeTestImpl.java
@@ -0,0 +1,128 @@
+package org.appverse.web.showcases.gwtshowcase.backend.services.presentation.impl.test;
+
+import org.appverse.web.framework.backend.api.helpers.test.AbstractTransactionalTest;
+import org.appverse.web.showcases.gwtshowcase.backend.converters.p2b.UserP2BBeanConverter;
+import org.appverse.web.showcases.gwtshowcase.backend.model.business.User;
+import org.appverse.web.showcases.gwtshowcase.backend.model.presentation.UserVO;
+import org.appverse.web.showcases.gwtshowcase.backend.services.business.RoleService;
+import org.appverse.web.showcases.gwtshowcase.backend.services.business.UserService;
+import org.appverse.web.showcases.gwtshowcase.backend.services.presentation.UserServiceFacade;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+import java.util.List;
+
+public class UserServiceFacadeTestImpl extends AbstractTransactionalTest {
+
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private RoleService roleService;
+
+	private User user;
+
+	@Autowired
+	private UserP2BBeanConverter userP2BBeanConverter;
+
+	@Autowired
+	private UserServiceFacade userServiceFacade;
+
+	private void checkUserVOList(final List<UserVO> list) {
+        for(UserVO userVO : list){
+            checkUserVOAllFields(userVO);
+        }
+	}
+
+	public void checkUserVOAllFields(final UserVO user) {
+		Assert.assertNotNull("Field can't be null", user.getCreated());
+		Assert.assertNotNull("Field can't be null", user.getCreatedBy());
+		Assert.assertNotNull("Field can't be null", user.getEmail());
+		Assert.assertNotNull("Field can't be null", user.getId());
+		Assert.assertNotNull("Field can't be null", user.getLastName());
+		Assert.assertNotNull("Field can't be null", user.getName());
+		Assert.assertNotNull("Field can't be null", user.getPassword());
+		Assert.assertNotNull("Field can't be null", user.isActive());
+		Assert.assertNotNull("Field can't be null", user.getUpdated());
+		Assert.assertNotNull("Field can't be null", user.getUpdatedBy());
+		Assert.assertNotNull("Field can't be null", user.getVersion());
+	}
+
+	public void checkUserVOAllFieldsWithCollections(final UserVO user) {
+
+		checkUserVOAllFields(user);
+
+		// Check here dependencies (collections, etc)
+
+        // Roles
+		Assert.assertNotNull("Roles collection can't be null", user.getRoles());
+		Assert.assertEquals("Roles collection size must be 5", 5, user.getRoles().size());
+	}
+
+	@Before
+	public void initialize() throws Exception {
+		// Set ALL first level fields
+		user = new User();
+		userSetAllFields(user);
+
+		// Add collections and dependencies
+
+        // We add all (5) existing roles
+		user.setRoles(roleService.loadRoles());
+	}
+
+	@Test
+	public void loadUser() throws Exception {
+		long id = userService.saveUser(user);
+		UserVO userVOAux = userServiceFacade.loadUser(id);
+
+		// Check object and relations
+		checkUserVOAllFieldsWithCollections(userVOAux);
+	}
+
+	@Test
+	public void loadUsers() throws Exception {
+		userService.saveUser(user);
+		List<UserVO> usersList = userServiceFacade.loadUsers();
+        // Remember that loadUsers() retrieves users with 'WithoutDependencies' scope so dependencies
+        // are not checked here
+        checkUserVOList(usersList);
+	}
+
+	@Test
+	public void saveUser() throws Exception {
+		// New persist
+		UserVO userVO = userP2BBeanConverter.convert(user);
+		long id = userServiceFacade.saveUser(userVO);
+		UserVO userVOAux = userServiceFacade.loadUser(id);
+		checkUserVOAllFieldsWithCollections(userVOAux);
+
+		// Update
+		userVOAux.setName("new");
+		id = userServiceFacade.saveUser(userVOAux);
+		userVOAux = userServiceFacade.loadUser(id);
+		Assert.assertNotNull(
+				"VO has not been able to be retrieved after update", userVOAux);
+		Assert.assertEquals("Invalid name in VO object", "new",
+				userVOAux.getName());
+		checkUserVOAllFieldsWithCollections(userVOAux);
+	}
+
+	public void userSetAllFields(final User user) {
+		// We don't set audit fields as they are automatically created
+		// We don't set id and version fields either as this are database
+		// assigned values
+        user.setStatus("A");
+        user.setCreated(new Date());
+        user.setCreatedBy("test");
+        user.setUpdated(new Date());
+        user.setUpdatedBy("test");
+		user.setActive(true);
+		user.setEmail("email");
+		user.setLastName("lastName");
+		user.setName("name");
+		user.setPassword("password");
+	}
+}
