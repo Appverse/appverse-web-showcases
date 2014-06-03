c048cb4533b8cec251f8e71762bb870367617bdc
marcelogft <mocr@gft.com>
2014-06-03 16:37:21 +0200

    JavaDoc

--- appverse-web-showcases-ws/appverse-web-showcases-ws-service/src/main/java/org/appverse/web/showcases/ws/service/AccountService.java
+++ appverse-web-showcases-ws/appverse-web-showcases-ws-service/src/main/java/org/appverse/web/showcases/ws/service/AccountService.java
@@ -8,12 +8,33 @@ import javax.jws.soap.SOAPBinding.Use;
 
 import org.appverse.web.showcases.ws.service.model.Account;
  
-//Service Endpoint Interface
+/**
+ * Account Service Endpoint Interface
+ * @author MOCR
+ *
+ */
 @WebService
 @SOAPBinding(style = Style.DOCUMENT, use=Use.LITERAL)
 public interface AccountService {
+	/**
+	 * Get Account by Client ID
+	 * @param clientId
+	 * @return account as string
+	 */
 	@WebMethod public String getAccountByClient(String clientId);
-	@WebMethod public String [] getAccounts(); 
+	/**
+	 * Get array of accounts as strings
+	 * @return String[] 
+	 */
+	@WebMethod public String [] getAccounts();
+	/**
+	 * Get Account 
+	 * @return Account
+	 */	
 	@WebMethod public Account getAccount();
+	/**
+	 * Get a list of Accounts
+	 * @return Account[]
+	 */
 	@WebMethod public Account [] getAccountObjects();
 }
